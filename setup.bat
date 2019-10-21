a11254c1612ac8beaa944980f86ae8395433b8c1
rrrobo <rrrobo@icloud.com>
2019-10-21 12:52:00 +0100

    add fonts directory

--- setup.bat
+++ setup.bat
@@ -2,6 +2,7 @@
 echo "Create the necessary directories"
 mkdir logs
 mkdir TechnicalDocument
+mkdir fonts
 echo "Download the latest version of the image from Docker Cloud"
 docker pull ryorobo/rcj-rescue-scoring-japan:latest
 echo "Create data storage for MongoDB"
